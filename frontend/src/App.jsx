f80318db6024a5240495536e16ad0c6292829270
Gourav Gupta <sorenpinky96@gmail.com>
2025-12-17 07:14:49 +0000

    used react-router-dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+
+import HomePage from './pages/Home'
+import LoginPage from './pages/LoginPage'
+import RegisterPage from './pages/RegisterPage'
 
 const App = () => {
   return (
     <>
       <h1>Header</h1>
-      {"body"}
+      
+        <Routes>
+          <Route path="/" Component={HomePage} />
+          <Route path="/login" Component={LoginPage} />
+          <Route path="/register" Component={RegisterPage} />
+        </Routes>
+
       <h1>Footer</h1>
     </>
   )
